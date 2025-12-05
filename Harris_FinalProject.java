35487dcdc6cb009b7811c7fc6763754a9230691d
KingMeeper <charr135@emich.edu>
2025-12-05 00:06:45 -0500

    working code and comments

--- Harris_FinalProject.java
+++ Harris_FinalProject.java
@@ -1,46 +1,80 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Harris_FinalProject {
 
-    static void printMenu(){
-            System.out.println("");
-            System.out.println("1. Attack");
-            System.out.println("2. Heal");
+    //Menu for choosing your turn
+    static int readMenuChoice(Scanner kb){
+        System.out.println("");
+        System.out.println("1. Attack");
+        System.out.println("2. Heal");
+        return kb.nextInt();
+    }
 
-        }
     public static void main(String[] args) {
-
+        //Entering Pokémon name and Boss name
         Scanner kb = new Scanner(System.in);
         System.out.print("Enter 'Pokémon' Name: ");
         String PokémonName = kb.next();
         System.out.print("Enter Boss Name: ");
         String BossName = kb.next();
 
-        Pokémon boss = new Pokémon(100, 10);
-        Pokémon player = new Pokémon(50, 30);
+        //Name, Health, Minimum Attack Power, Maximum Attack Power, Minimum Recovery, and Maximum Recovery for the player and the boss.
+        Pokémon boss = new Pokémon(PokémonName, 2500, 50, 250, 10, 100);
+        Pokémon player = new Pokémon(BossName, 1500, 25, 500, 25, 500);
 
+        //Menu Choice
         int choice = 0;
 
-        while (PokémonHealth > 0 && BossHealth > 0){
-            do {
-            printMenu();
+        //Confirms that either the player or the boss is alive
+        boolean pokémonAlive = true;
+        boolean bossAlive = true;
+
+        //Allows the boss to take his turn
+        Random bossTurn = new Random(System.currentTimeMillis());
+        
+        //Keeps the game running as long as either the Pokémon or the Boss is still alive
+        while (pokémonAlive && bossAlive){
+            // player turn portion
+            boss.printInfo();
+            player.printInfo();
             choice = readMenuChoice(kb);
             if (choice == 1){
-
+                int attack = player.nextAttack();
+                System.out.println(PokémonName + " attacked " + BossName + "." + "It did " + attack + " damage");
+                boolean died = boss.attacked(attack);
+                if (died) {
+                    bossAlive = false;
+                }    
             } else if (choice == 2){
-
+                int heal = player.heal();
+                System.out.println(PokémonName + " recovered " + heal + " health");
             } else {
                 System.out.println(PokémonName + " chose to forfeit their turn!");
             }
+
+            // boss turn portion
+            choice = bossTurn.nextInt(10);
+            if (choice <= 7){
+                int attack = boss.nextAttack();
+                System.out.println(BossName + " attacked " + PokémonName + "." + "It did " + attack + " damage");
+                boolean died = player.attacked(attack);
+                if (died) {
+                    pokémonAlive = false;
+                }    
+            } else {
+                int heal = boss.heal();
+                System.out.println(BossName + " recovered " + heal + " health");
+            } 
         }
-        }
-        if (PokémonHealth <= 0){
+        //Messages that appear if you win or lose to the boss
+        if (!pokémonAlive){
                 System.out.println(PokémonName + " has blacked out!");
                 System.out.println("Game Over!");
             }
-            if (BossHealth <= 0){
+            if (!bossAlive){
                 System.out.println(BossName + " has perished!");
-                System.out.println("Congragulations! You Win!");
+                System.out.println("Congradulations! You Win! :)");
             }
     }
     
--- Pokémon.java
+++ Pokémon.java
@@ -1,37 +1,58 @@
 import java.util.Random;
 public class Pokémon {
 
-    int health;
+    //Statistics for the player and boss
+    private String name;
+    private int health;
+    private int attackMin;
+    private int attackMax;
+    private int healMin;
+    private int healMax;
 
-    int attackMin = 25;
-    int attackMax = 100;
-    int healMin = 10;
-    int healMax = 300;
-
+    //Random system that allows me to generate random numbers for attacking and healing
     Random random = new Random(System.currentTimeMillis());
 
-    public Pokémon(int attackMin, int attackMax, int healMin, int healMax){
+    //Constructor for the Pokémon and boss
+    public Pokémon(String name, int health, int attackMin, int attackMax, int healMin, int healMax){
+        this.name = name;
+        this.health = health;
         this.attackMin = attackMin;
         this.attackMax = attackMax;
         this.healMin = healMin;
         this.healMax = healMax;
     }
 
+    //Generates a random amount of damage to be applied to either the player or the boss
     public int nextAttack(){
         return random.nextInt(attackMax - attackMin + 1) + attackMin;
     }
 
+    //Applies the amount of damage to either the player or the boss
     public boolean attacked(int attack){
         health -= attack;
         return health <= 0;
     }
 
-    public void heal(){
-        health += random.nextInt(healMax - healMin + 1) + healMin;
+    //Gets a random amount of health to be recovered, and add it to the player that chose to recover
+    public int heal(){
+        int h = random.nextInt(healMax - healMin + 1) + healMin;
+        health += h;
+        return h;
     }
 
-    
+    //Getter that sets up the printInfo(), which allows players to see the status of the boss and the player.
+    public int getHealth(){
+        return health;
+    }
 
+    //Getter that sets up the printInfo(), which allows players to see the status of the boss and the player
+    public String getName(){
+        return name;
+    }
 
+    //Prints the status about the player and boss before each turn.
+    public void printInfo(){
+      System.out.println(name + " has " + health + " health");
+    }
 
 }
